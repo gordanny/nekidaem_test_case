e4cc46919db9a6c830f92e417199c7ef33af5750
Denis Gorshkov <gordanny.social@gmail.com>
2021-07-15 23:26:12 +0500

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if len(self.text) > 50:
+            return f'{self.text[:50]}...'
+        return self.text
